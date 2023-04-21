aad0689119c5c864ff9510e7d92553ac1abbf851
malggie <malg@int.pl>
2023-04-21 10:07:58 +0200

    Added tasks 2-6

--- AMG/src/main/java/org/example/App.java
+++ AMG/src/main/java/org/example/App.java
@@ -8,6 +8,110 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        /* Zadanie 2:
+        Stwórz tablicę nazw europejskich miast i następnie wypisz je na następujące sposoby:
+        od początku do końca
+        od końca do początku
+        co drugie miasto */
+
+        String[] cities = new String[4];
+        cities[0] = "Berlin";
+        cities[1] = "Kraków";
+        cities[2] = "Paryż";
+        cities[3] = "Wiedeń";
+
+        for (Integer i = 0; i < cities.length; i++) {
+            String city = cities[i];
+            System.out.println(city);
+        }
+        System.out.println();
+
+        for (Integer i = 3; i >= 0 ; i--) {
+            String newCity = cities[i];
+            System.out.println(newCity);
+        }
+        System.out.println();
+
+        for (Integer i = 0; i < cities.length; i+=2) {
+            String city = cities[i];
+            System.out.println(city);
+        }
+
+        /* Zadanie 3:
+        Stwórz tablicę liczb zmiennoprzecinkowych i następnie oblicz sumę oraz średnią tych liczb. */
+
+        Float[] floats = new Float[] {4.05f , 2.3f , 8.94f , -1.0f};
+
+        Float sum = floats[0] + floats[1] + floats[2] + floats[3];
+        Float average = sum/ floats.length;
+        System.out.println(sum);
+        System.out.println(average);
+
+
+        /* Zadanie 4:
+        Stwórz tablicę imion (z powtórzeniami) i następnie policz, ile razy występuje w niej imię "Jan". */
+
+        String[] names = new String[] {"Jan", "Ania", "Jan"};
+//
+//        int iterator = 0;
+//        for (int i = 0; i < names.length; i++) {
+//            String name = names[i];
+//            if (name.equals("Jan")) {
+//                iterator++;
+//                System.out.println(name);
+//            }
+//        }
+//        System.out.println("Imię Jan występuje " + iterator + " razy");
+
+
+        /* Zadanie 5:
+        Stwórz tablicę imion (bez powtórzeń) i następnie podaj, na którym indexie znajduje się w niej imię "Jan". */
+
+        String[] names = new String[] {"Janek", "Jan", "Jasiek"};
+
+        for (int i = 0; i < names.length; i++) {
+            String name = names[i];
+            if (name.equals("Jan")) {
+                System.out.println("Imię " + name + " znajduje się na " + i + " indeksie");
+            }
+        }
+
+        /* Zadanie 6:
+        Stwórz tablicę liczb całkowitych i następnie znajdź największy i najmniejszy element tej tablicy. */
+
+        Integer[] numbers = new Integer[] {1, 4, 6, 10};
+
+        if (numbers[0] > numbers[1] && numbers[0] >numbers[2] && numbers[0] > numbers[3]) {
+            System.out.println("Największy element tablicy to " + numbers[0]);
+        }
+        else if (numbers[1] > numbers[0] && numbers[1] >numbers[2] && numbers[1] > numbers[3]) {
+            System.out.println("Największy element tablicy to " + numbers[1]);
+        }
+
+        else if (numbers[2] > numbers[0] && numbers[2] >numbers[1] && numbers[2] > numbers[3]) {
+            System.out.println("Największy element tablicy to " + numbers[2]);
+        }
+
+        else {
+            System.out.println("Największy element tablicy to " + numbers[3]);
+        }
+
+
+        if (numbers[0] < numbers[1] && numbers[0] < numbers[2] && numbers[0] < numbers[3]) {
+            System.out.println("Najmniejszy element tablicy to " + numbers[0]);
+        }
+        else if (numbers[1] < numbers[0] && numbers[1] < numbers[2] && numbers[1] < numbers[3]) {
+            System.out.println("Najmniejszy element tablicy to " + numbers[1]);
+        }
+
+        else if (numbers[2] < numbers[0] && numbers[2] < numbers[1] && numbers[2] < numbers[3]) {
+            System.out.println("Najmniejszy element tablicy to " + numbers[2]);
+        }
+
+        else {
+            System.out.println("Najmniejszy element tablicy to " + numbers[3]);
+        }
+
     }
 }
